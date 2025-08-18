ae47cd09b2352c75ea656b1f14e3abe7c3796b46
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-08-18 11:58:57 -0300

    first commit

---
+++ src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package br.com.fiap.financewalk.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.financewalk.model.Category;
+
+@RestController
+public class CategoryController {
+    
+    @GetMapping("/categories")
+    public List<Category> index() {
+
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category(1L, "Lazer", "movie"));
+        categories.add(new Category(2L, "Transporte", "bus"));
+        categories.add(new Category(3L, "Educação", "book"));
+        
+        return categories;
+    }
+
+}
