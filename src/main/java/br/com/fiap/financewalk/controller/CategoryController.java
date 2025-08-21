607ce5ad5dc47ae30e8fca0f160fe2e8aa2d543f
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-08-21 09:38:20 -0300

    cirando metodo post

--- src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
+++ src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
@@ -1,25 +1,44 @@
 package br.com.fiap.financewalk.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.financewalk.model.Category;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class CategoryController {
+
+    private List<Category> repo = List.of(
+        new Category(1L, "Lazer", "Popcorn"),
+        new Category(2L, "Transporte", "Bus"),
+        new Category(3L, "Educação", "Book")
+    ).stream()
+    .collect(Collectors.toList());
     
     @GetMapping("/categories")
     public List<Category> index() {
+        return repo;
+    }
 
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1L, "Lazer", "movie"));
-        categories.add(new Category(2L, "Transporte", "bus"));
-        categories.add(new Category(3L, "Educação", "book"));
-        
-        return categories;
+    @PostMapping("/categories")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Category create(@RequestBody Category category) {
+        category.setId(new Random().nextLong());
+        log.info("Criando categoria: " + category);
+        repo.add(category);
+        return category;
     }
 
 }
