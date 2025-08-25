8c0c3a1f3a1d1758e0885e244aa0eb235857524b
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-08-25 11:44:18 -0300

    delete e get by id craidos

--- src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
+++ src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
@@ -4,16 +4,18 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.financewalk.model.Category;
+import jakarta.websocket.server.PathParam;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -21,12 +23,11 @@ import lombok.extern.slf4j.Slf4j;
 public class CategoryController {
 
     private List<Category> repo = List.of(
-        new Category(1L, "Lazer", "Popcorn"),
-        new Category(2L, "Transporte", "Bus"),
-        new Category(3L, "Educação", "Book")
-    ).stream()
-    .collect(Collectors.toList());
-    
+            new Category(1L, "Lazer", "Popcorn"),
+            new Category(2L, "Transporte", "Bus"),
+            new Category(3L, "Educação", "Book")).stream()
+            .collect(Collectors.toList());
+
     @GetMapping("/categories")
     public List<Category> index() {
         return repo;
@@ -39,6 +40,23 @@ public class CategoryController {
         log.info("Criando categoria: " + category);
         repo.add(category);
         return category;
+    }
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<Category> get(@PathVariable Long id) {
+        log.info("Buscando categoria pelo id: " + id);
+
+        return ResponseEntity.of(
+                repo.stream()
+                        .filter(c -> c.getId() == id)
+                        .findFirst());
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.info("Apagando categoria por id: {}", id);
+        repo.removeIf(c -> c.getId() == id);
+        return ResponseEntity.noContent().build();
     }
 
 }
