b3b54750035219c2bfaa93cd49cab8778db7e6fa
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-08-28 09:40:17 -0300

    conectando a base de dados

--- src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
+++ src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.financewalk.model.Category;
-import jakarta.websocket.server.PathParam;
+import br.com.fiap.financewalk.repository.CategoryRepo;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -26,23 +27,19 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("categories")
 public class CategoryController {
 
-    private List<Category> repo = List.of(
-            new Category(1L, "Lazer", "Popcorn"),
-            new Category(2L, "Transporte", "Bus"),
-            new Category(3L, "Educação", "Book")).stream()
-            .collect(Collectors.toList());
+    @Autowired
+    private CategoryRepo repo;
 
     @GetMapping
     public List<Category> index() {
-        return repo;
+        return repo.findAll();
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Category create(@RequestBody Category category) {
-        category.setId(new Random().nextLong());
         log.info("Criando categoria: " + category);
-        repo.add(category);
+        repo.save(category);
         return category;
     }
 
@@ -53,27 +50,25 @@ public class CategoryController {
         return ResponseEntity.of(getCategoryById(id));
     }
 
-    private Optional<Category> getCategoryById(Long id) {
-        return repo.stream()
-                .filter(c -> c.getId() == id)
-                .findFirst();
-    }
-
     @DeleteMapping("{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         log.info("Apagando categoria por id: {}", id);
-        repo.remove(getCategoryById(id).orElse(null));
+        repo.delete(getCategoryById(id).orElse(null));
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("{id}")
     public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody Category updCategory) {
         log.info("Atualizando categoria: {}, com id: {}", updCategory, id);
-        repo.remove(getCategoryById(id).orElseThrow());
+
         updCategory.setId(id);
-        repo.add(updCategory);
+        repo.save(updCategory);
         return ResponseEntity.ok().build();
 
+    }
+
+        private Optional<Category> getCategoryById(Long id) {
+        return repo.findById(id);
     }
 
 }
--- src/main/java/br/com/fiap/financewalk/model/Category.java
+++ src/main/java/br/com/fiap/financewalk/model/Category.java
@@ -1,12 +1,22 @@
 package br.com.fiap.financewalk.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+// @Table(name = "tb_fw_category")
 public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
     private String icon;
     
