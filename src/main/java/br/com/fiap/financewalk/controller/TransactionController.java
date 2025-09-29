82f5edbe5f9b661c5af9276f9abfb7af005e844a
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-09-29 12:04:21 -0300

    quase certo

--- src/main/java/br/com/fiap/financewalk/controller/TransactionController.java
+++ src/main/java/br/com/fiap/financewalk/controller/TransactionController.java
@@ -1,13 +1,18 @@
 package br.com.fiap.financewalk.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,17 +30,27 @@ public class TransactionController {
     TransactionRepo repo;
 
     @GetMapping
-    public List<Transaction> index() {
-        return repo.findAll();
+    public List<Transaction> index(@RequestParam(required = false) String description,
+            @RequestParam(required = false) LocalDate date) {
+
+        var matcher = ExampleMatcher.matchingAll()
+                .withIgnoreNullValues()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        var example = Example.of(Transaction.builder()
+                .description(description)
+                .date(date)
+                .build(), matcher);
+
+        return repo.findAll(example);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Transaction create(@RequestBody @Valid Transaction transaction) {
-        log.info("criando transação: {}" , transaction);
+        log.info("criando transação: {}", transaction);
         return repo.save(transaction);
     }
 
-    
-    
 }
